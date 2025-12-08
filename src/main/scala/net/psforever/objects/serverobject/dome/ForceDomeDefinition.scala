2222c50339c6ae1e5368b544faf863739dd8c13f
Fate-JH <Jason_DiDonato@yahoo.com>
2025-12-07 23:39:43 -0500

    adding entity to represent force dome, and wiring force dome to capitol facility; touching the force dome while it is active causes death on both server and client

---
+++ src/main/scala/net/psforever/objects/serverobject/dome/ForceDomeDefinition.scala
@@ -0,0 +1,9 @@
+// Copyright (c) 2025 PSForever
+package net.psforever.objects.serverobject.dome
+
+import net.psforever.objects.serverobject.structures.AmenityDefinition
+
+class ForceDomeDefinition(objectId: Int)
+  extends AmenityDefinition(objectId) {
+  Name = "force_dome"
+}
