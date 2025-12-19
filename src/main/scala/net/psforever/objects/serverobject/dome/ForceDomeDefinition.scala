dd4cdc6ff29cf5c925f7fe544fc2be32cbddeef5
Fate-JH <Jason_DiDonato@yahoo.com>
2025-12-18 19:29:06 -0500

    the force dome exhibits a perimeter in which enemies will be destroyed when it energizes; the facility generator will become undestroyable when the force dome is energized

--- src/main/scala/net/psforever/objects/serverobject/dome/ForceDomeDefinition.scala
+++ src/main/scala/net/psforever/objects/serverobject/dome/ForceDomeDefinition.scala
@@ -2,8 +2,20 @@
 package net.psforever.objects.serverobject.dome
 
 import net.psforever.objects.serverobject.structures.AmenityDefinition
+import net.psforever.types.Vector3
 
 class ForceDomeDefinition(objectId: Int)
   extends AmenityDefinition(objectId) {
   Name = "force_dome"
+  /** offsets that define the perimeter of the pyramidal force "dome" barrier;
+   * these points are the closest to where the dome interacts with the ground at a corner;
+   * should be sequential, either clockwise or counterclockwise */
+  private var perimeter: List[Vector3] = List()
+
+  def PerimeterOffsets: List[Vector3] = perimeter
+
+  def PerimeterOffsets_=(points: List[Vector3]): List[Vector3] = {
+    perimeter = points
+    PerimeterOffsets
+  }
 }
